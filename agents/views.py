0769be1f6374a96c4e695cbc4ed536c5618253cf
areskadi <iliyasali2107@gmail.com>
2022-06-04 01:13:01 +0600

    added password reset

--- agents/views.py
+++ agents/views.py
@@ -1,4 +1,6 @@
+import random
 from django.shortcuts import render, reverse
+from django.core.mail import send_mail
 
 from .mixins import OrganisorAndLoginRequiredMixin
 
@@ -22,9 +24,22 @@ class AgentCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agent-list")
 
     def form_valid(self, form):
-        agent = form.save(commit=False)
-        agent.organisation = self.request.user.userprofile
-        agent.save()
+        user = form.save(commit=False)
+        user.is_agent = True
+        user.is_organisor = False
+        user.set_password(f"{random.randint(0, 1000000)}")
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation = self.request.user.userprofile
+        )
+
+        send_mail(
+            subject="Your are invited to be an agent",
+            message = "You were added as an agent on DJCRM. Please come login",
+            from_email = "admin@test.com",
+            recipient_list=[user.email]
+        )
         return super(AgentCreateView, self).form_valid(form)
 
 
--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+    )
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import LandingPageView, SignupView
@@ -11,9 +18,11 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name = 'landing-page'),
     path('leads/', include('leads.urls', namespace='leads')),
     path('agents/', include('agents.urls', namespace='agents')),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('signup/', SignupView.as_view(), name='signup')
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
 
 if settings.DEBUG:
